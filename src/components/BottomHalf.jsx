bd073ba9b3f4163ea97b8723a5ed2e84bc97d4ab
matkronh <mats.kodehode@gmail.com>
2023-02-22 13:49:15 +0100

    updated hover effects

--- src/components/BottomHalf.jsx
+++ src/components/BottomHalf.jsx
@@ -1,8 +1,11 @@
 import styles from './styles/styles.module.css'
 import TopHalfMini from './mini/TopHalfMini'
 import { Link, Outlet } from 'react-router-dom'
+import './styles/styles.module.css'
 
 function BottomHalf(){
+
+    
     return(
         <div>
             <br/>
@@ -15,14 +18,14 @@ function BottomHalf(){
                 <div className={styles.borderThree}></div>
                 <div className={styles.borderFour}></div>
 
-                <div className={styles.brec8}><Link to='/'><TopHalfMini /></Link></div>
+                <div className={`${styles.brec8} + ${styles.hvrpulseshrink}`}><Link to='/'><TopHalfMini /></Link></div>
 
-                <Link className={styles.brec2} to='/utdanning'><br/><h2 className={styles.brectext}>Utdanning</h2></Link>
-                <Link className={styles.brec3} to='/skills'><br/><h2 className={styles.brectext2}>Skills</h2></Link>
-                <Link className={styles.brec4} to='/portfolio'><br/><h2 className={styles.brectext2}>Portfolio</h2></Link>
-                <Link className={styles.brec5} to='/referanse'> <br/><h2 className={styles.brectext3}>Referanse</h2></Link>
-                <Link className={styles.brec6} to='/erfaring'><br/><h2 className={styles.brectext2}>Erfaring</h2></Link>
-                <Link className={styles.brec7} to='/ommeg'><br/><h2 className={styles.brectext2}>Om Meg</h2></Link>
+                <Link className={`${styles.brec2} + ${styles.hvrwobbletop}`} to='/utdanning'><br/><h2 className={styles.brectext}>Utdanning</h2></Link>
+                <Link className={`${styles.brec3} + ${styles.hvrwobbletop}`} to='/skills'><br/><h2 className={styles.brectext2}>Skills</h2></Link>
+                <Link className={`${styles.brec4} + ${styles.hvrwobbletop}`} to='/portfolio'><br/><h2 className={styles.brectext2}>Portfolio</h2></Link>
+                <Link className={`${styles.brec5} + ${styles.hvrwobbletop}`} to='/referanse'> <br/><h2 className={styles.brectext3}>Referanse</h2></Link>
+                <Link className={`${styles.brec6} + ${styles.hvrwobbletop}`} to='/erfaring'><br/><h2 className={styles.brectext2}>Erfaring</h2></Link>
+                <Link className={`${styles.brec7} + ${styles.hvrwobbletop}`} to='/ommeg'><br/><h2 className={styles.brectext2}>Om Meg</h2></Link>
             </div>
             
         </div>
