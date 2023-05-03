981d03543220b6c5aa40db01c57e7a0e1b02b930
matkronh <mats.kodehode@gmail.com>
2023-05-03 14:26:49 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,5 @@
-/* import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom' */
 import BottomHalf from "./components/BottomHalf"
 import styles from './components/styles/styles.module.css'
-/* import Ommeg from './components/routes/ommeg'
-import Utdanning from './components/routes/utdanning'
-import Skills from './components/routes/skills'
-import Portfoliox from './components/routes/portfolio'
-import Referanse from './components/routes/referanse'
-import Erfaring from './components/routes/erfaring'
-import EmptySpace from './components/EmptySpace' */
-
-
-/* 
-  BottomHalf = layout
-  Tophalf = Landing Page
- */
 
 function App() {
 
