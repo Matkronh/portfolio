042433ecc764442e6122d3cd623a5e4d41b418bd
matkronh <mats.kodehode@gmail.com>
2023-06-05 09:10:04 +0200

    d

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -320,25 +320,11 @@ function BottomHalf(){
                         <div className={styles.parallaximg}>
                             <Parallax blur={3} bgImage={parall2} bgImageAlt="code snippet" bgClassName={styles.parallaximg} strength={-400}>
                                 <div className={styles.sectionheight2}>
-                                    <Utdanning
-                                    education={state ? "Min Utdanning" : "My Education"}
-                                    edu1={state ? "1. Sandsli VGS - Hotel and Resturantfag" : "1. Sandsli VGS - Hotell- og resturant"}
-                                    edu2={state ? "2. Krokeide VGS - IKT-Servicefag VG2": "2. Krokeide VGS - IKT-Service VG2"}
-                                    edu3={state ? "3. LINK A2G Åsane - Påbygging til generell studiekompetanse VG3" : "LINK A2G Åsane - University Admission Certification VG3"}
-                                    edu4={state ? "4. Kodehode AMO Kurs" : "4. Kodehode AMO Bootcamp"}
-                                    />
-                                    <Erfaring 
-                                    experience={state ? "Min Erfaring" : "My Experience"}
-                                    exp1={state ? "1. Binders IKT avdeling - IKT medarbeider" : "1. Binders IKT department - IKT employee"}
-                                    exp2={state ? "2. ISS AS - Hjelpearbeider" : "2. ISS AS - Auxiliary worker"}
-                                    exp3={state ? "3. Sharecat Solutions - Junior Konsultant" : "3. Sharecat Solutions - Junior Consultant"}
-                                    />
-                                    <Referanse 
-                                    reference={state ? "Mine Referanser" : "My References"}
-                                    ref1={state ? "Referanse 1" : "Reference 1"}
-                                    ref2={state ? "Referanse 2" : "Reference 2"}
-                                    ref3={state ? "Referanse 3" : "Reference 3"}
-                                    ref4={state ? "Referanse 4" : "Reference 4"}
+                                    <Ommeg
+                                        aboutmetitle={state ? "Om Meg" : "About Me"}
+                                        para1={state ? "Jeg heter Mats og jeg er 33 år ung. Jeg elsker å programmere, designe og bruke kreativiteten min til å skape, om det er gjennom html, css eller js- til syvende og sist skal alt flettes sammen og det er noe med den følelsen når alt klikker sammen jeg rett og slett ikke får nok av. Når jeg får på meg headsettet, utestenger alle distraksjoner og kommer inn i sonen så føler jeg at det begynner å krible i fingrene og det er rett opp i syvende gir." : "My name is Mats and I am 33 years young. I love programming, designing and using my creativity to create, wether it's through html, css or js- at the end of the day everything has to be interconnected and it's something about the feeling when everything clicks I just can't get enough of. When I get my headset on, shut out the distractions and get into the zone I feel my hands tingling and it's straight into seventh gear."}
+                                        para2={state ? "Jeg liker foruten å bruke fritiden min til bl.a spill og level design i ulike arenaer, har vært innom Unity der jeg selv-lærte meg en del c#, men mesteparten av tiden de siste årene har jeg brukt i Source SDK der jeg har designet og bygget opp spill leveler; noe som hjalp meg veldig når jeg begynte med Javascript siden det gikk veldig mye i logikk, I/O, parenting, class & id i Source SDK og." : "Coding aside, I like to devote my free time to game and level design in a variety of different arenas such as Unity where I self taught myself a lot of c# and published a simple game on the playstore but the majority of my time over the last years has gone into Source SDK where I have designed and built up game maps which has been a valuable resource for me when I started with Javascript since it touches on many similar topics such as Logic, I/O, parenting, classes & ID's."}
+                                        para3={state ? "Denne fritidshobbyen min har uten tvil gjort meg til en bedre utvikler med timesvis av feilsøking, tilbakemeldinger og hardt arbeid." : "This hobby of mine has without doubt made me a better developer with hours of troubleshooting, feedback and hard work."}
                                     />
                                 </div>
                             </Parallax>
@@ -359,15 +345,34 @@ function BottomHalf(){
                             project3text={state ? "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing" : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing"}
                             project3link={"www.example-link.com"}
                             />
-                        </div>
+                            </div>
+                            <div className={styles.sectionheight3}>
+                            <div className={styles.cardsection}>
+                                <Utdanning
+                                    education={state ? "Min Utdanning" : "My Education"}
+                                    edu1={state ? "1. Sandsli VGS - Hotel and Resturantfag" : "1. Sandsli VGS - Hotell- og resturant"}
+                                    edu2={state ? "2. Krokeide VGS - IKT-Servicefag VG2": "2. Krokeide VGS - IKT-Service VG2"}
+                                    edu3={state ? "3. LINK A2G Åsane - Påbygging til generell studiekompetanse VG3" : "LINK A2G Åsane - University Admission Certification VG3"}
+                                    edu4={state ? "4. Kodehode AMO Kurs" : "4. Kodehode AMO Bootcamp"}
+                                    />
+                                    <Erfaring 
+                                    experience={state ? "Min Erfaring" : "My Experience"}
+                                    exp1={state ? "1. Binders IKT avdeling - IKT medarbeider" : "1. Binders IKT department - IKT employee"}
+                                    exp2={state ? "2. ISS AS - Hjelpearbeider" : "2. ISS AS - Auxiliary worker"}
+                                    exp3={state ? "3. Sharecat Solutions - Junior Konsultant" : "3. Sharecat Solutions - Junior Consultant"}
+                                    />
+                                    <Referanse 
+                                    reference={state ? "Mine Referanser" : "My References"}
+                                    ref1={state ? "Referanse 1" : "Reference 1"}
+                                    ref2={state ? "Referanse 2" : "Reference 2"}
+                                    ref3={state ? "Referanse 3" : "Reference 3"}
+                                    ref4={state ? "Referanse 4" : "Reference 4"}
+                                    />
+                            </div>
+                            </div>
+                        
                             <div className={styles.transition2}/>
                                 <div className={styles.sectionheight}>
-                                    <Ommeg
-                                        aboutmetitle={state ? "Om Meg" : "About Me"}
-                                        para1={state ? "Jeg heter Mats og jeg er 33 år ung. Jeg elsker å programmere, designe og bruke kreativiteten min til å skape, om det er gjennom html, css eller js- til syvende og sist skal alt flettes sammen og det er noe med den følelsen når alt klikker sammen jeg rett og slett ikke får nok av. Når jeg får på meg headsettet, utestenger alle distraksjoner og kommer inn i sonen så føler jeg at det begynner å krible i fingrene og det er rett opp i syvende gir." : "My name is Mats and I am 33 years young. I love programming, designing and using my creativity to create, wether it's through html, css or js- at the end of the day everything has to be interconnected and it's something about the feeling when everything clicks I just can't get enough of. When I get my headset on, shut out the distractions and get into the zone I feel my hands tingling and it's straight into seventh gear."}
-                                        para2={state ? "Jeg liker foruten å bruke fritiden min til bl.a spill og level design i ulike arenaer, har vært innom Unity der jeg selv-lærte meg en del c#, men mesteparten av tiden de siste årene har jeg brukt i Source SDK der jeg har designet og bygget opp spill leveler; noe som hjalp meg veldig når jeg begynte med Javascript siden det gikk veldig mye i logikk, I/O, parenting, class & id i Source SDK og." : "Coding aside, I like to devote my free time to game and level design in a variety of different arenas such as Unity where I self taught myself a lot of c# and published a simple game on the playstore but the majority of my time over the last years has gone into Source SDK where I have designed and built up game maps which has been a valuable resource for me when I started with Javascript since it touches on many similar topics such as Logic, I/O, parenting, classes & ID's."}
-                                        para3={state ? "Denne fritidshobbyen min har uten tvil gjort meg til en bedre utvikler med timesvis av feilsøking, tilbakemeldinger og hardt arbeid." : "This hobby of mine has without doubt made me a better developer with hours of troubleshooting, feedback and hard work."}
-                                    />
                                 </div>
 
         </div>
